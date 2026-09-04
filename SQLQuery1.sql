b491a7cb9e5735a984d037b066ce954557f1a030
nurlanssu <nurlanssu@code.edu.az>
2026-09-04 12:32:02 +0400

    added change

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -103,3 +103,36 @@ SELECT * FROM Actors;
 SELECT * FROM Genres;
 
 SELECT * FROM Movies;
+
+INSERT INTO MovieActors(MovieId, ActorId)
+VALUES
+(1, 1), -- The Dark Knight - Christian Bale
+(1, 2), -- The Dark Knight - Heath Ledger
+(2, 3), -- The Godfather - Marlon Brando
+(3, 4), -- Inception - Leonardo DiCaprio
+(4, 5); -- Fight Club - Brad Pitt
+
+INSERT INTO MovieDirectors(DirectorId, MovieId)
+VALUES
+(1, 1),
+(2, 2),
+(1, 3),
+(1, 5);
+
+INSERT INTO MovieGenres(MovieId, GenreId)
+VALUES
+(1, 1),
+(1, 2),
+(2, 2),
+(2, 3),
+(3, 1),
+(3, 4),
+(4, 2),
+(4, 3),
+(5, 3),
+(5, 4);
+
+SELECT * FROM MovieActors;
+SELECT * FROM MovieDirectors;
+SELECT * FROM MovieGenres;
+
